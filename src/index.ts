cad562fcc06b0b9af11376235db9463f9b151663
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-10-26 23:09:24 +0100

    feat: initial sse implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log(process.argv);
+import * as eventSource from 'eventsource';
+
+// console.clear();
+
+const token = 'test token goes here';
+const sse = new eventSource('https://server.nqframework.com/v1/actions/receive?token=' + token);
+// const sse = new eventSource('http://localhost:3000/v1/actions/receive?token=' + token);
+sse.addEventListener('message', (event) => {
+    console.log('data ', event);
+});
+// sse.onmessage = (event) => {
+//     console.log('data ',event);
+// }
+console.log('started up');
